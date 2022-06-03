0d375338411ff81461190d7bf76fd08891131e4b
syahru <syahruragaramdhani@gmail.com>
2022-06-04 00:12:25 +0700

    menambah file php

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -32,7 +32,9 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
-
+$routes->get('/about', 'Page::about');
+$routes->get('/contact', 'Page::contact');
+$routes->get('/faqs', 'Page::faqs');
 /*
  * --------------------------------------------------------------------
  * Additional Routing
